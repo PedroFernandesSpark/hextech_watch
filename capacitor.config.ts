905c78da7158512286a6e8001be2b2851e7edf91
Pedro Spark Fernandes <pedrofernandesspark@gmail.com>
2025-02-23 17:25:54 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bluetoothApp',
+  webDir: 'dist'
+};
+
+export default config;
