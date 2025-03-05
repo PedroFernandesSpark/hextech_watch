4a4b6fe1a5d9f9e3a87a364a77a8b0b21c4732c9
Pedro Spark Fernandes <pedrofernandesspark@gmail.com>
2025-03-05 09:18:55 -0300

    WIP

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,127 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Bluetooth App</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content>
+      <ion-button @click="scanDevices">Search Devices</ion-button>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <ion-list>
+        <ion-item v-for="device in devices" :key="device.uuids" v-if="device[0]">
+          {{ device.name || 'Unnamed' }} - {{ device.uuids }}
+        </ion-item>
+      </ion-list>
+
+      <ion-card v-if="dataFromDevice">
+        <ion-card-header>
+          Connected to: HEXTECH WATCH
+        </ion-card-header>
+        <ion-card-content>
+          Data from device: {{ dataFromDevice }}
+        </ion-card-content>
+      </ion-card>
+
+      <div v-if="chartData.datasets[0].data.length">
+        <LineChart :chart-data="chartData" :chart-options="chartOptions" />
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonItem, IonCard, IonCardHeader, IonCardContent } from '@ionic/vue';
+import { BleClient } from '@capacitor-community/bluetooth-le';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Line } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, PointElement, LinearScale, CategoryScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, LinearScale, CategoryScale);
+
+export default {
+  components: {
+    IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonItem, IonCard, IonCardHeader, IonCardContent, LineChart: Line
+  },
+  data() {
+    return {
+      devices: [],
+      dataFromDevice: '',
+      chartData: {
+        labels: [],
+        datasets: [{
+          label: 'Bluetooth Data',
+          data: [],
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  async created() {
+    await BleClient.initialize({ androidNeverForLocation: true });
+    console.log('Bluetooth LE initialized');
+  },
+  methods: {
+    async scanDevices() {
+      try {
+        await BleClient.requestLEScan({ name: 'HEXTECH_WATCH' }, async (e) => {
+          if (e) this.devices.push(e);
+        });
+        
+        await BleClient.connect("F0:24:F9:57:5C:8E");
+        let i = 0;
+        while (i <= 700) {
+          const data = await BleClient.read(
+            "F0:24:F9:57:5C:8E",
+            "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+            "e5a1d466-344c-4be3-ab3f-189f80dd7518"
+          );
+          
+          this.dataFromDevice = new TextDecoder().decode(data);
+          this.updateChartData(parseFloat(this.dataFromDevice));
+          await this.saveDataToFile(this.dataFromDevice);
+          i++;
+        }
+      } catch (error) {
+        console.error('Error during scan:', error);
+      }
+    },
+    async saveDataToFile(data) {
+      try {
+        await Filesystem.appendFile({
+          path: 'bluetooth_data.txt',
+          data: data + '\n',
+          directory: Directory.Documents,
+          encoding: Encoding.UTF8
+        });
+        console.log('Data saved:', data);
+      } catch (error) {
+        console.error('Error saving file:', error);
+      }
+    },
+    updateChartData(value) {
+      if (!isNaN(value)) {
+        this.chartData.labels.push(new Date().toLocaleTimeString());
+        this.chartData.datasets[0].data.push(value);
+        if (this.chartData.labels.length > 20) {
+          this.chartData.labels.shift();
+          this.chartData.datasets[0].data.shift();
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+div {
+  height: 300px;
+  width: 100%;
 }
 </style>
